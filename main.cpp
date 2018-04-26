10dcbfdbcc65f953844f44440eaa66d19c8f930a
Junyoung Kim <junkim327@gmail.com>
2018-04-26 04:44:51 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by JunYoung Kim on 2018. 4. 11..
+//  Copyright © 2018년 Junyoung Kim. All rights reserved.
+//
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+class LRUCache {
+private:
+    int _capacity;
+    list<int> queue;
+public:
+    LRUCache(int capacity) : _capacity(capacity) {}
+    
+    int get(int key) {
+        
+    }
+    
+    void put(int key, int value) {
+        
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
